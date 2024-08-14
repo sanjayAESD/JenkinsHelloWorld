c871ec6d72e76ea20bf8eaa737bb676b01d65f72
sanjayAESD <100312255+sanjayAESD@users.noreply.github.com>
2024-08-14 17:49:36 +0530

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello World and Welcome to Jenkins Training");
+		System.out.println("Hello World and Welcome to Jenkins Training and made some changes");
 
 	}
 
